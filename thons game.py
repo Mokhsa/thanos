a4ec913332bfe06ba532ad828047d0e80b4d69e3
Mokhsa <mohamedsalman22710@gmail.com>
2023-06-22 17:10:16 +0300

    add projects file

---
+++ thons game.py
@@ -0,0 +1,24 @@
+import os 
+import random
+import time
+
+
+def get_files(p):
+    return(os.listdir(p))
+
+def destroy(path): 
+    '''
+    parameters
+    path: path to the directory you want to destroy
+    delete half of the files in the directory
+    
+    '''
+    Folderlength=len(get_files(path))//2
+    for i in range(Folderlength):
+        file_name=random.choice(get_files(path))
+        os.remove(f"{path}/{file_name}")
+        time.sleep(1)
+        print(f"{file_name} destroyed")
+        
+        
+destroy('universe')
